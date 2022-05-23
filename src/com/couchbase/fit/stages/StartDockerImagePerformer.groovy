5222e1949790ab3ea25f2057c289a6c4af9862fd
charlie-hayes <charlie.hayes@couchbase.com>
2022-05-23 10:21:59 +0100

    Troubleshooting

--- src/com/couchbase/fit/stages/StartDockerImagePerformer.groovy
+++ src/com/couchbase/fit/stages/StartDockerImagePerformer.groovy
@@ -26,19 +26,19 @@ class StartDockerImagePerformer extends Stage {
     void executeImpl(StageContext ctx) {
         try {
             //TODO change "performer" to an image name or something, the driver isnt playing well with having the image name as a name atm
-            ctx.env.execute("172.23.104.43:2376 docker kill performer")
+            ctx.env.execute("DOCKER_HOST=172.23.104.43:2376 docker kill performer")
         }
         catch(RuntimeException err) {
         }
 
         //TODO change "performer" to an image name or something, the driver isnt playing well with having the image name as a name atm
-        ctx.env.execute("172.23.104.43:2376 docker run --rm --network perf -d -p $port:8060 --name performer $imageName version=$version")
+        ctx.env.execute("DOCKER_HOST=172.23.104.43:2376 docker run --rm --network perf -d -p $port:8060 --name performer $imageName version=$version")
     }
 
     @Override
     void finishImpl(StageContext ctx) {
         //TODO change "performer" to an image name or something, the driver isnt playing well with having the image name as a name atm
-        ctx.env.execute("172.23.104.43:2376 docker kill performer")
+        ctx.env.execute("DOCKER_HOST=172.23.104.43:2376 docker kill performer")
         // uncomment this when uploading to CI
         //ctx.env.execute("docker system prune")
     }
